62d5dd8690f12714a81f59ee1513a0a7882a2a57
SH <sh.ylzhxx@163.com>
2018-08-20 17:42:27 +0800

    fix some bugs

--- library/src/main/java/com/shxhzhxx/sdk/activity/BaseActivity.java
+++ library/src/main/java/com/shxhzhxx/sdk/activity/BaseActivity.java
@@ -7,7 +7,12 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.shxhzhxx.imageloader.ImageLoader;
+import com.shxhzhxx.sdk.Net;
+
 public class BaseActivity extends DownloadActivity implements View.OnClickListener {
+    protected String TAG = this.getClass().getSimpleName();
+    protected String IDENTIFY = String.valueOf(hashCode());
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,6 +24,8 @@ public class BaseActivity extends DownloadActivity implements View.OnClickListen
     protected void onDestroy() {
         super.onDestroy();
         ActivityManager.remove(this);
+        Net.getInstance().cancelByTag(IDENTIFY);
+        ImageLoader.getInstance().cancelByTag(IDENTIFY);
     }
 
     protected void setOnClickListener(View[] views) {
@@ -38,11 +45,11 @@ public class BaseActivity extends DownloadActivity implements View.OnClickListen
 
     }
 
-    protected void setStatusBarColor(int color, boolean dark) {
+    protected void setStatusBarColor(int color, boolean lightStatusBar) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             Window window = getWindow();
             int visibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
-            if (dark) {
+            if (lightStatusBar) {
                 visibility |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
             }
             window.getDecorView().setSystemUiVisibility(visibility);
