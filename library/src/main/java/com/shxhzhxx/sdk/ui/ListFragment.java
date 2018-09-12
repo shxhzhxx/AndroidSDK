964081a3fc510969d14ef6b0412a396c4f9081af
SH <sh.ylzhxx@163.com>
2018-09-12 13:41:42 +0800

    fix listFragment bug

--- library/src/main/java/com/shxhzhxx/sdk/ui/ListFragment.java
+++ library/src/main/java/com/shxhzhxx/sdk/ui/ListFragment.java
@@ -106,9 +106,13 @@ public abstract class ListFragment<M, VH extends RecyclerView.ViewHolder, A exte
             nextPage(REFRESH);
     }
 
-    protected abstract int pageSize();
+    protected int pageSize() {
+        return 10;
+    }
 
-    protected abstract int pageStartAt();
+    protected int pageStartAt() {
+        return 0;
+    }
 
     @NonNull
     protected abstract A onAdapter();
@@ -134,7 +138,8 @@ public abstract class ListFragment<M, VH extends RecyclerView.ViewHolder, A exte
     private void nextPage(final int flag) {
         mEnableLoadMore = false;
         mSwipe.setEnabled(false);
-        final int page = pageStartAt() + (flag & (REFRESH | SWIPE)) != 0 ? 0 : mList.size() / pageSize();
+        final int page = pageStartAt() +
+                ((flag & (REFRESH | SWIPE)) != 0 ? 0 : mList.size() / pageSize());
 
         if ((flag & (SWIPE | LOAD_MORE)) == 0) {
             mSwipe.setRefreshing(true);
