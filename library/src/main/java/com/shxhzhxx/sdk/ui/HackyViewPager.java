450df8c9c5ea6d560d15c64e80ca6b5da10c9864
SH <sh.ylzhxx@163.com>
2019-03-08 18:04:33 +0800

    fix crush bug of ImageViewerActivity

---
+++ library/src/main/java/com/shxhzhxx/sdk/ui/HackyViewPager.java
@@ -0,0 +1,28 @@
+package com.shxhzhxx.sdk.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.viewpager.widget.ViewPager;
+
+public class HackyViewPager extends ViewPager {
+    public HackyViewPager(@NonNull Context context) {
+        super(context);
+    }
+
+    public HackyViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
